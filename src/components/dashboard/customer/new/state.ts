4e8588d43dc0d68eb41010c6fabafc44c5f580f6
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-03-19 18:11:02 -0700

    add New: state

---
+++ src/components/dashboard/customer/new/state.ts
@@ -0,0 +1,10 @@
+import { NewState } from "./types";
+
+const initialNewState: NewState = {
+    barLineChartYAxisVariable: "overview",
+    barLineChartKind: "bar",
+    calendarChartYAxisVariable: "total",
+    pieChartYAxisVariable: "overview",
+};
+
+export { initialNewState };
