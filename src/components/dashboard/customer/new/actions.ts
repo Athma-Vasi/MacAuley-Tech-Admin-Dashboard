a261d3062ea1cff5c7c3f66f41e0c539b5f9d4ad
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-03-19 18:10:14 -0700

    add New: actions

---
+++ src/components/dashboard/customer/new/actions.ts
@@ -0,0 +1,16 @@
+type NewAction = {
+    setBarLineChartYAxisVariable: "setBarLineChartYAxisVariable";
+    setBarLineChartKind: "setBarLineChartKind";
+    setCalendarChartYAxisVariable: "setCalendarChartYAxisVariable";
+    setPieChartYAxisVariable: "setPieChartYAxisVariable";
+};
+
+const newAction: NewAction = {
+    setBarLineChartYAxisVariable: "setBarLineChartYAxisVariable",
+    setBarLineChartKind: "setBarLineChartKind",
+    setCalendarChartYAxisVariable: "setCalendarChartYAxisVariable",
+    setPieChartYAxisVariable: "setPieChartYAxisVariable",
+};
+
+export { newAction };
+export type { NewAction };
