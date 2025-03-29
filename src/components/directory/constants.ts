8a72b6fd5cec8188f2ca90668eee664bb1c9bba6
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-03-29 13:07:50 -0700

    add Directory, d3Tree

---
+++ src/components/directory/constants.ts
@@ -0,0 +1,19 @@
+import { CheckboxRadioSelectData, Department } from "../../types";
+
+const DEPARTMENT_DATA: CheckboxRadioSelectData<Department> = [
+    { value: "Executive Management", label: "Executive Management" },
+    { value: "Human Resources", label: "Human Resources" },
+    { value: "Store Administration", label: "Store Administration" },
+    { value: "Office Administration", label: "Office Administration" },
+    { value: "Accounting", label: "Accounting" },
+    { value: "Sales", label: "Sales" },
+    { value: "Marketing", label: "Marketing" },
+    { value: "Information Technology", label: "Information Technology" },
+    { value: "Repair Technicians", label: "Repair Technicians" },
+    { value: "Field Service Technicians", label: "Field Service Technicians" },
+    { value: "Logistics and Inventory", label: "Logistics and Inventory" },
+    { value: "Customer Service", label: "Customer Service" },
+    { value: "Maintenance", label: "Maintenance" },
+];
+
+export { DEPARTMENT_DATA };
