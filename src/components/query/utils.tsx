920129499c0b95e1a8c786c65e19788d1da1895f
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-04-17 18:44:26 -0700

    fix query: utils

--- src/components/query/utils.tsx
+++ src/components/query/utils.tsx
@@ -1,22 +1,19 @@
 import { Flex, Stack, Text, Title } from "@mantine/core";
-import {
-    VALIDATION_FUNCTIONS_TABLE,
-    type ValidationKey,
-} from "../../constants/validations";
+
 import type {
     CheckboxRadioSelectData,
     InputType,
-    StepperChild,
-    StepperPage,
     Validation,
 } from "../../types";
 import { splitCamelCase } from "../../utils";
+import { VALIDATION_FUNCTIONS_TABLE, ValidationKey } from "../../validations";
+
 import {
     BOOLEAN_OPERATOR_DATA,
     COMPARISON_OPERATORS_DATA,
     IN_OPERATOR_DATA,
 } from "./constants";
-import type {
+import {
     FilterInputsType,
     GeneralSearchCase,
     MongoComparisonOperator,
@@ -24,39 +21,6 @@ import type {
     QueryOperator,
     SortInputsType,
 } from "./types";
-
-function addInputsToStepperPages(stepperPages: StepperPage[]): StepperPage[] {
-    // createdAt and updatedAt are guaranteed to exist in all models and are used as initial values for filter & sort
-    // username and userId exist in all models and are used as initial values for search
-    // the fields must be added here because the stepper pages are used to populate and validate query inputs
-
-    const createdAtInput: StepperChild = {
-        inputType: "date",
-        name: "createdAt",
-        validationKey: "dateNearPast",
-    };
-
-    const updatedAtInput: StepperChild = {
-        inputType: "date",
-        name: "updatedAt",
-        validationKey: "dateNearPast",
-    };
-
-    const usernameInput: StepperChild = {
-        inputType: "text",
-        name: "username",
-        validationKey: "username",
-    };
-
-    const clonedStepperPages = structuredClone(stepperPages);
-    clonedStepperPages[0].children.push(
-        createdAtInput,
-        updatedAtInput,
-        usernameInput,
-    );
-
-    return clonedStepperPages;
-}
 
 type OperatorsInputType = {
     operators: CheckboxRadioSelectData<QueryOperator>;
@@ -82,108 +46,6 @@ type QueryInputsData = {
     /** Map<field names, validationKey> */
     inputsValidationsMap: InputsValidationsMap;
 };
-
-/**
- * - Extracts the names, input data from the stepper pages and used in the corresponding query sections
- */
-function createQueryInputsData(stepperPages: StepperPage[]): QueryInputsData {
-    // data created with these input types can have a subset of operators applied to them
-
-    const filterInputsTypeSet = new Set<FilterInputsType>([
-        "boolean", // 'equal to' operator
-        "date", // comparison operators
-        "number", // comparison operators
-        "select", // 'in' operator
-        "time", // comparison operators
-    ]);
-    const sortInputsSet = new Set<SortInputsType>(["date", "number", "time"]); // can apply sort: ascending | descending
-
-    const validatedInputsSet = new Set<InputType>([
-        "text",
-        "date",
-        "number",
-        "time",
-    ]);
-
-    const initialAcc: QueryInputsData = {
-        fieldNamesOperatorsTypesMap: new Map<string, OperatorsInputType>(),
-        filterFieldSelectInputData: [],
-        selectInputsDataMap: new Map<string, CheckboxRadioSelectData>(),
-        projectionCheckboxData: [],
-        searchFieldSelectInputData: [],
-        sortFieldSelectData: [],
-        inputsValidationsMap: new Map(),
-    };
-
-    const stepperPagesWithAddedInputs = addInputsToStepperPages(stepperPages);
-    return stepperPagesWithAddedInputs.reduce<QueryInputsData>((acc, page) => {
-        const {
-            fieldNamesOperatorsTypesMap,
-            filterFieldSelectInputData,
-            projectionCheckboxData,
-            selectInputsDataMap,
-            searchFieldSelectInputData,
-            sortFieldSelectData,
-            inputsValidationsMap,
-        } = acc;
-
-        page.children.forEach((child) => {
-            const { inputType, name, selectInputData, validationKey } = child;
-
-            const checkboxRadioSelectData = {
-                label: splitCamelCase(name),
-                value: name,
-            };
-
-            const comparisonOperatorsInputsSet = new Set<InputType>([
-                "date",
-                "number",
-                "time",
-            ]); // can apply comparison operators
-
-            const operatorsInputTypeObject = {
-                inputType,
-                operators: comparisonOperatorsInputsSet.has(inputType)
-                    ? COMPARISON_OPERATORS_DATA
-                    : inputType === "boolean"
-                    ? BOOLEAN_OPERATOR_DATA
-                    : IN_OPERATOR_DATA,
-            } as OperatorsInputType;
-            fieldNamesOperatorsTypesMap.set(name, operatorsInputTypeObject);
-
-            projectionCheckboxData.push(checkboxRadioSelectData);
-
-            if (filterInputsTypeSet.has(inputType as FilterInputsType)) {
-                filterFieldSelectInputData.push(checkboxRadioSelectData);
-            }
-
-            if (inputType === "text") {
-                searchFieldSelectInputData.push(checkboxRadioSelectData);
-            }
-
-            if (sortInputsSet.has(inputType as SortInputsType)) {
-                sortFieldSelectData.push(checkboxRadioSelectData);
-            }
-
-            if (selectInputData !== undefined) {
-                selectInputsDataMap.set(name, selectInputData);
-            }
-
-            if (validatedInputsSet.has(inputType)) {
-                if (validationKey === undefined) {
-                    return acc;
-                }
-
-                inputsValidationsMap.set(name, {
-                    validationKey,
-                    validation: VALIDATION_FUNCTIONS_TABLE[validationKey],
-                });
-            }
-        });
-
-        return acc;
-    }, initialAcc);
-}
 
 function removeProjectionExclusionFields(
     projectionExclusionFields: string[],
@@ -573,7 +435,6 @@ const PROJECTION_HELP_MODAL_CONTENT = (
 );
 
 export {
-    createQueryInputsData,
     createQueryString,
     FILTER_HELP_MODAL_CONTENT,
     GENERAL_SEARCH_HELP_MODAL_CONTENT,
