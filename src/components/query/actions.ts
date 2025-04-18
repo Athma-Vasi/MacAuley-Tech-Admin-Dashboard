4e043b89718297522711ff23b3ecda8464f98b83
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-04-17 18:38:32 -0700

    add query: actions

---
+++ src/components/query/actions.ts
@@ -0,0 +1,40 @@
+type QueryAction = {
+    modifyQueryChains: "modifyQueryChains";
+    setFilterComparisonOperator: "setFilterComparisonOperator";
+    setFilterComparisonOperatorSelectData:
+        "setFilterComparisonOperatorSelectData";
+    setFilterField: "setFilterField";
+    setFilterLogicalOperator: "setFilterLogicalOperator";
+    setFilterValue: "setFilterValue";
+    setGeneralSearchCase: "setGeneralSearchCase";
+    setGeneralSearchExclusionValue: "setGeneralSearchExclusionValue";
+    setGeneralSearchInclusionValue: "setGeneralSearchInclusionValue";
+    setIsError: "setIsError";
+    setIsSearchDisabled: "setIsSearchDisabled";
+    setLimitPerPage: "setLimitPerPage";
+    setProjectionExclusionFields: "setProjectionExclusionFields";
+    setSortDirection: "setSortDirection";
+    setSortField: "setSortField";
+};
+
+const queryAction: QueryAction = {
+    modifyQueryChains: "modifyQueryChains",
+    setFilterComparisonOperator: "setFilterComparisonOperator",
+    setFilterComparisonOperatorSelectData:
+        "setFilterComparisonOperatorSelectData",
+    setFilterField: "setFilterField",
+    setFilterLogicalOperator: "setFilterLogicalOperator",
+    setFilterValue: "setFilterValue",
+    setGeneralSearchCase: "setGeneralSearchCase",
+    setGeneralSearchExclusionValue: "setGeneralSearchExclusionValue",
+    setGeneralSearchInclusionValue: "setGeneralSearchInclusionValue",
+    setIsError: "setIsError",
+    setIsSearchDisabled: "setIsSearchDisabled",
+    setLimitPerPage: "setLimitPerPage",
+    setProjectionExclusionFields: "setProjectionExclusionFields",
+    setSortDirection: "setSortDirection",
+    setSortField: "setSortField",
+};
+
+export { queryAction };
+export type { QueryAction };
