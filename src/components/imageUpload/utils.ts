71d9b3728e12434bf80f0004cc793458f2ebbc99
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-05-01 08:32:39 -0700

    add imageUpload: utils

---
+++ src/components/imageUpload/utils.ts
@@ -0,0 +1,40 @@
+import { ModifiedFile } from "../AccessibleFileInput";
+
+function validateImages({
+    allowedFileExtensionsRegex,
+    imageFileBlobs,
+    maxImageSize,
+    maxImagesAmount,
+}: {
+    allowedFileExtensionsRegex: RegExp;
+    imageFileBlobs: Array<ModifiedFile>;
+    maxImageSize: number;
+    maxImagesAmount: number;
+}) {
+    const areImagesInvalid = imageFileBlobs.length > maxImagesAmount ||
+        imageFileBlobs.reduce((invalidAcc, fileBlob) => {
+            if (fileBlob === null) {
+                return true;
+            }
+
+            const { size, type } = fileBlob;
+            if (size > maxImageSize) {
+                return true;
+            }
+
+            if (!type.length) {
+                return true;
+            }
+
+            const extension = type.split("/")[1];
+            if (!allowedFileExtensionsRegex.test(extension)) {
+                return true;
+            }
+
+            return invalidAcc;
+        }, false);
+
+    return { areImagesInvalid };
+}
+
+export { validateImages };
