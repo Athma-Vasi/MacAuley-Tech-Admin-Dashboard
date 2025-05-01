dfa2dca71a2606a10da54d3fe174de231430c932
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-05-01 08:32:08 -0700

    add imageUpload: state

---
+++ src/components/imageUpload/state.ts
@@ -0,0 +1,13 @@
+import { MAX_IMAGES } from "./constants";
+import { AccessibleImageInputState } from "./types";
+
+const initialAccessibleImageInputState: AccessibleImageInputState = {
+    currentImageIndex: 0,
+    fileNames: [],
+    imageFileBlobs: [],
+    isLoading: false,
+    orientations: Array.from({ length: MAX_IMAGES }, () => 1),
+    qualities: Array.from({ length: MAX_IMAGES }, () => 10),
+};
+
+export { initialAccessibleImageInputState };
