d2e2dd87880aa5af13f3821c9985ebe9b22f38f8
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-05-16 19:55:47 -0700

    refactor action to mapped type

--- src/components/login/actions.ts
+++ src/components/login/actions.ts
@@ -1,12 +1,13 @@
-type LoginAction = {
-  setErrorMessage: "setErrorMessage";
-  setIsLoading: "setIsLoading";
-  setIsSubmitting: "setIsSubmitting";
-  setIsSuccessful: "setIsSuccessful";
-  setLoginFetchWorker: "setLoginFetchWorker";
-  setPassword: "setPassword";
-  setUsername: "setUsername";
-};
+import { Prettify } from "../../types";
+import { LoginState } from "./types";
+
+type LoginAction = Prettify<
+  {
+    [K in keyof LoginState as `set${Capitalize<string & K>}`]: `set${Capitalize<
+      string & K
+    >}`;
+  }
+>;
 
 const loginAction: LoginAction = {
   setErrorMessage: "setErrorMessage",
