86bab710a6bb80d298f03f6ae2fca7afb77e9710
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-05-16 19:59:28 -0700

    refactor action to mapped type

--- src/components/dashboard/product/actions.ts
+++ src/components/dashboard/product/actions.ts
@@ -1,10 +1,12 @@
-type ProductMetricsAction = {
-  setCalendarChartsData: "setCalendarChartsData";
-  setCards: "setCards";
-  setCharts: "setCharts";
-  setIsGenerating: "setIsGenerating";
-  setProductChartsWorker: "setProductChartsWorker";
-};
+import { Prettify } from "../../../types";
+import { ProductMetricsState } from "./types";
+
+type ProductMetricsAction = Prettify<
+  {
+    [K in keyof ProductMetricsState as `set${Capitalize<string & K>}`]:
+      `set${Capitalize<string & K>}`;
+  }
+>;
 
 const productMetricsAction: ProductMetricsAction = {
   setCalendarChartsData: "setCalendarChartsData",
--- src/components/dashboard/product/rus/actions.ts
+++ src/components/dashboard/product/rus/actions.ts
@@ -1,10 +1,15 @@
-type RUSAction = {
-  setBarLineRadialChartKind: "setBarLineRadialChartKind";
-  setYAxisKey: "setYAxisKey";
-};
+import { Prettify } from "../../../../types";
+import { RUSState } from "./types";
+
+type RUSAction = Prettify<
+  {
+    [K in keyof RUSState as `set${Capitalize<string & K>}`]: `set${Capitalize<
+      string & K
+    >}`;
+  }
+>;
 
 const rusAction: RUSAction = {
-  setBarLineRadialChartKind: "setBarLineRadialChartKind",
   setYAxisKey: "setYAxisKey",
 };
 
