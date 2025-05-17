aec17f00526cb2f2001b79744c4a4bbc6646ed6f
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-05-16 19:51:28 -0700

    refactor action to mapped type

--- src/components/register/actions.ts
+++ src/components/register/actions.ts
@@ -1,38 +1,12 @@
-type RegisterAction = {
-  setActiveStep: "setActiveStep";
-  setAddressLine: "setAddressLine";
-  setCheckEmailWorker: "setCheckEmailWorker";
-  setCheckUsernameWorker: "setCheckUsernameWorker";
-  setCity: "setCity";
-  setConfirmPassword: "setConfirmPassword";
-  setCountry: "setCountry";
-  setDepartment: "setDepartment";
-  setEmail: "setEmail";
-  setErrorMessage: "setErrorMessage";
-  setFilesInError: "setFilesInError";
-  setFirstName: "setFirstName";
-  setFormData: "setFormData";
-  setInputsInError: "setInputsInError";
-  setIsEmailExists: "setIsEmailExists";
-  setIsEmailExistsSubmitting: "setIsEmailExistsSubmitting";
-  setIsError: "setIsError";
-  setIsSubmitting: "setIsSubmitting";
-  setIsSuccessful: "setIsSuccessful";
-  setIsUsernameExists: "setIsUsernameExists";
-  setIsUsernameExistsSubmitting: "setIsUsernameExistsSubmitting";
-  setJobPosition: "setJobPosition";
-  setLastName: "setLastName";
-  setPassword: "setPassword";
-  setPostalCodeCanada: "setPostalCodeCanada";
-  setPostalCodeUS: "setPostalCodeUS";
-  setProfilePictureUrl: "setProfilePictureUrl";
-  setProvince: "setProvince";
-  setState: "setState";
-  setStepsInError: "setStepsInError";
-  setStepsWithEmptyInputs: "setStepsWithEmptyInputs";
-  setStoreLocation: "setStoreLocation";
-  setUsername: "setUsername";
-};
+import { Prettify } from "../../types";
+import { RegisterState } from "./types";
+
+type RegisterAction = Prettify<
+  {
+    [K in keyof RegisterState as `set${Capitalize<string & K>}`]:
+      `set${Capitalize<string & K>}`;
+  }
+>;
 
 const registerAction: RegisterAction = {
   setActiveStep: "setActiveStep",
