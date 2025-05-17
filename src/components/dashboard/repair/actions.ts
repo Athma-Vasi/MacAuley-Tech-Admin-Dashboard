473dd65985f2acdaaf6003f781e38ffc995dbc70
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-05-16 19:58:06 -0700

    refactor action to mapped type

--- src/components/dashboard/repair/actions.ts
+++ src/components/dashboard/repair/actions.ts
@@ -1,10 +1,12 @@
-type RepairMetricsAction = {
-  setCalendarChartsData: "setCalendarChartsData";
-  setCards: "setCards";
-  setCharts: "setCharts";
-  setIsGenerating: "setIsGenerating";
-  setRepairChartsWorker: "setRepairChartsWorker";
-};
+import { Prettify } from "../../../types";
+import { RepairMetricsState } from "./types";
+
+type RepairMetricsAction = Prettify<
+  {
+    [K in keyof RepairMetricsState as `set${Capitalize<string & K>}`]:
+      `set${Capitalize<string & K>}`;
+  }
+>;
 
 const repairMetricsAction: RepairMetricsAction = {
   setCalendarChartsData: "setCalendarChartsData",
