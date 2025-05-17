57a8f2ec2c7e614002a04ef7c57b66c84964b719
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-05-16 19:58:34 -0700

    refactor action to mapped type

--- src/components/dashboard/repair/repairRUS/actions.ts
+++ src/components/dashboard/repair/repairRUS/actions.ts
@@ -1,6 +1,12 @@
-type RepairRUSAction = {
-  setYAxisKey: "setYAxisKey";
-};
+import { Prettify } from "../../../../types";
+import { RepairRUSState } from "./types";
+
+type RepairRUSAction = Prettify<
+  {
+    [K in keyof RepairRUSState as `set${Capitalize<string & K>}`]:
+      `set${Capitalize<string & K>}`;
+  }
+>;
 
 const repairRUSAction: RepairRUSAction = {
   setYAxisKey: "setYAxisKey",
