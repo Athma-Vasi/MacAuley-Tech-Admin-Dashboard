34a7f18e916b84840b537fc4df852cefb08b3f29
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-05-16 20:05:24 -0700

    refactor action to mapped type

--- src/components/charts/responsiveSunburstChart/actions.ts
+++ src/components/charts/responsiveSunburstChart/actions.ts
@@ -1,48 +1,14 @@
-type ResponsiveSunburstChartAction = {
-  // base
-  setCornerRadius: "setCornerRadius";
+import { Prettify } from "../../../types";
+import { ResponsiveSunburstChartState } from "./types";
 
-  // margin
-  setMarginTop: "setMarginTop";
-  setMarginRight: "setMarginRight";
-  setMarginBottom: "setMarginBottom";
-  setMarginLeft: "setMarginLeft";
-
-  // style
-  setChartBorderColor: "setChartBorderColor";
-  setChartBorderWidth: "setChartBorderWidth";
-  setChartColors: "setChartColors";
-  setEnableFillPatterns: "setEnableFillPatterns";
-  setFillPatterns: "setFillPatterns";
-  setInheritColorFromParent: "setInheritColorFromParent";
-
-  // arc labels
-  setArcLabelsRadiusOffset: "setArcLabelsRadiusOffset";
-  setArcLabelsSkipAngle: "setArcLabelsSkipAngle";
-  setArcLabelsTextColor: "setArcLabelsTextColor";
-  setEnableArcLabels: "setEnableArcLabels";
-  setArcLabel: "setArcLabel";
-
-  // motion
-  setEnableAnimate: "setEnableAnimate";
-  setMotionConfig: "setMotionConfig";
-  setTransitionMode: "setTransitionMode";
-
-  // options
-  setChartTitle: "setChartTitle";
-  setChartTitleColor: "setChartTitleColor";
-  setChartTitlePosition: "setChartTitlePosition";
-  setChartTitleSize: "setChartTitleSize";
-
-  // screenshot
-  setScreenshotFilename: "setScreenshotFilename";
-  setScreenshotImageQuality: "setScreenshotImageQuality";
-  setScreenshotImageType: "setScreenshotImageType";
-
-  // reset all
-  resetChartToDefault: "resetChartToDefault";
-  setIsError: "setIsError";
-};
+type ResponsiveSunburstChartAction = Prettify<
+  {
+    [K in keyof ResponsiveSunburstChartState as `set${Capitalize<string & K>}`]:
+      `set${Capitalize<string & K>}`;
+  } & {
+    resetChartToDefault: "resetChartToDefault";
+  }
+>;
 
 const responsiveSunburstChartAction: ResponsiveSunburstChartAction = {
   // base
