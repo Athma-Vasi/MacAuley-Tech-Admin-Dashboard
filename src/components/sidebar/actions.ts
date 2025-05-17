af69927047fbf3f9cfab00c9942867064b148c55
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-05-16 19:56:24 -0700

    refactor action to mapped type

--- src/components/sidebar/actions.ts
+++ src/components/sidebar/actions.ts
@@ -1,9 +1,12 @@
-type SidebarAction = {
-    setClickedNavlink: "setClickedNavlink";
-    setDirectoryFetchWorker: "setDirectoryFetchWorker";
-    setLogoutFetchWorker: "setLogoutFetchWorker";
-    setMetricsFetchWorker: "setMetricsFetchWorker";
-};
+import { Prettify } from "../../types";
+import { SidebarState } from "./types";
+
+type SidebarAction = Prettify<
+    {
+        [K in keyof SidebarState as `set${Capitalize<string & K>}`]:
+            `set${Capitalize<string & K>}`;
+    }
+>;
 
 const sidebarAction: SidebarAction = {
     setClickedNavlink: "setClickedNavlink",
