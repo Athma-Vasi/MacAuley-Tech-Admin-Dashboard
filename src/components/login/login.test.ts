efd2b072b6f7e382d73a5d55532e9b42f62a6598
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-05-29 19:24:59 -0700

    login: add unit tests

--- src/components/login/login.test.ts
+++ src/components/login/login.test.ts
@@ -4,20 +4,29 @@ import {
     INVALID_BOOLEANS,
     INVALID_STRINGS,
     VALID_BOOLEANS,
+    VALID_STRINGS,
 } from "../../constants";
 import { loginAction } from "./actions";
 import {
+    loginReducer_setCustomerMetricsWorker,
+    loginReducer_setErrorMessage,
+    loginReducer_setFinancialMetricsGenerated,
+    loginReducer_setFinancialMetricsWorker,
     loginReducer_setIsLoading,
     loginReducer_setIsSubmitting,
     loginReducer_setIsSuccessful,
     loginReducer_setLoginFetchWorker,
     loginReducer_setPassword,
+    loginReducer_setProductMetricsGenerated,
+    loginReducer_setProductMetricsWorker,
+    loginReducer_setRepairMetricsGenerated,
+    loginReducer_setRepairMetricsWorker,
     loginReducer_setUsername,
 } from "./reducers";
 import { initialLoginState } from "./state";
 
 describe("loginReducer", () => {
-    describe("setIsLoading", () => {
+    describe(loginAction.setIsLoading, () => {
         it("should allow valid boolean values", () => {
             VALID_BOOLEANS.forEach((value) => {
                 const dispatch = {
@@ -48,7 +57,7 @@ describe("loginReducer", () => {
         });
     });
 
-    describe("setIsSubmitting", () => {
+    describe(loginAction.setIsSubmitting, () => {
         it("should allow valid boolean values", () => {
             VALID_BOOLEANS.forEach((value) => {
                 const dispatch = {
@@ -80,7 +89,7 @@ describe("loginReducer", () => {
         });
     });
 
-    describe("setIsSuccessful", () => {
+    describe(loginAction.setIsSuccessful, () => {
         it("should allow valid boolean values", () => {
             VALID_BOOLEANS.forEach((value) => {
                 const dispatch = {
@@ -112,7 +121,7 @@ describe("loginReducer", () => {
         });
     });
 
-    describe("setPassword", () => {
+    describe(loginAction.setPassword, () => {
         it("should allow valid string values", () => {
             const validValues = [
                 "password123Q!",
@@ -149,7 +158,7 @@ describe("loginReducer", () => {
         });
     });
 
-    describe("setUsername", () => {
+    describe(loginAction.setUsername, () => {
         it("should allow valid string values", () => {
             const validValues = [
                 "username123Q!",
@@ -186,7 +195,7 @@ describe("loginReducer", () => {
         });
     });
 
-    describe("setLoginFetchWorker", () => {
+    describe(loginAction.setLoginFetchWorker, () => {
         it("should allow valid Worker values", () => {
             const worker = new Worker("");
             const dispatch = {
@@ -210,6 +219,255 @@ describe("loginReducer", () => {
                 dispatch,
             );
             expect(state.loginFetchWorker).toBeNull();
+        });
+    });
+
+    describe(loginAction.setErrorMessage, () => {
+        it("should allow valid string values", () => {
+            VALID_STRINGS.forEach((value) => {
+                const dispatch = {
+                    action: loginAction.setErrorMessage,
+                    payload: value,
+                };
+                const state = loginReducer_setErrorMessage(
+                    initialLoginState,
+                    dispatch,
+                );
+                expect(state.errorMessage).toBe(value);
+            });
+        });
+
+        it("should not allow invalid string values", () => {
+            const initialErrorMessage = initialLoginState.errorMessage;
+
+            INVALID_STRINGS.forEach((value) => {
+                const dispatch = {
+                    action: loginAction.setErrorMessage,
+                    payload: value,
+                };
+                const state = loginReducer_setErrorMessage(
+                    initialLoginState,
+                    dispatch as any,
+                );
+                expect(state.errorMessage).toBe(initialErrorMessage);
+            });
+        });
+    });
+
+    describe(loginAction.setCustomerMetricsWorker, () => {
+        it("should allow valid Worker values", () => {
+            const worker = new Worker("");
+            const dispatch = {
+                action: loginAction.setCustomerMetricsWorker,
+                payload: worker,
+            };
+            const state = loginReducer_setCustomerMetricsWorker(
+                initialLoginState,
+                dispatch,
+            );
+            expect(state.customerMetricsWorker).toBeInstanceOf(Worker);
+        });
+
+        it("should not allow invalid Worker values", () => {
+            const worker = {} as Worker;
+            const dispatch = {
+                action: loginAction.setCustomerMetricsWorker,
+                payload: worker,
+            };
+            const state = loginReducer_setCustomerMetricsWorker(
+                initialLoginState,
+                dispatch,
+            );
+            expect(state.customerMetricsWorker).toBeNull();
+        });
+    });
+
+    describe(loginAction.setFinancialMetricsGenerated, () => {
+        it("should allow valid boolean values", () => {
+            VALID_BOOLEANS.forEach((value) => {
+                const dispatch = {
+                    action: loginAction.setFinancialMetricsGenerated,
+                    payload: value,
+                };
+                const state = loginReducer_setFinancialMetricsGenerated(
+                    initialLoginState,
+                    dispatch,
+                );
+                expect(state.financialMetricsGenerated).toBe(value);
+            });
+        });
+
+        it("should not allow invalid boolean values", () => {
+            const initialFinancialMetricsGenerated =
+                initialLoginState.financialMetricsGenerated;
+
+            INVALID_BOOLEANS.forEach((value) => {
+                const dispatch = {
+                    action: loginAction.setFinancialMetricsGenerated,
+                    payload: value,
+                };
+                const state = loginReducer_setFinancialMetricsGenerated(
+                    initialLoginState,
+                    dispatch as any,
+                );
+                expect(state.financialMetricsGenerated).toBe(
+                    initialFinancialMetricsGenerated,
+                );
+            });
+        });
+    });
+
+    describe(loginAction.setFinancialMetricsWorker, () => {
+        it("should allow valid Worker values", () => {
+            const worker = new Worker("");
+            const dispatch = {
+                action: loginAction.setFinancialMetricsWorker,
+                payload: worker,
+            };
+            const state = loginReducer_setFinancialMetricsWorker(
+                initialLoginState,
+                dispatch,
+            );
+            expect(state.financialMetricsWorker).toBeInstanceOf(Worker);
+        });
+
+        it("should not allow invalid Worker values", () => {
+            const worker = {} as Worker;
+            const dispatch = {
+                action: loginAction.setFinancialMetricsWorker,
+                payload: worker,
+            };
+            const state = loginReducer_setFinancialMetricsWorker(
+                initialLoginState,
+                dispatch,
+            );
+            expect(state.financialMetricsWorker).toBeNull();
+        });
+    });
+
+    describe(loginAction.setProductMetricsGenerated, () => {
+        it("should allow valid boolean values", () => {
+            VALID_BOOLEANS.forEach((value) => {
+                const dispatch = {
+                    action: loginAction.setProductMetricsGenerated,
+                    payload: value,
+                };
+                const state = loginReducer_setProductMetricsGenerated(
+                    initialLoginState,
+                    dispatch,
+                );
+                expect(state.productMetricsGenerated).toBe(value);
+            });
+        });
+
+        it("should not allow invalid boolean values", () => {
+            const initialProductMetricsGenerated =
+                initialLoginState.productMetricsGenerated;
+
+            INVALID_BOOLEANS.forEach((value) => {
+                const dispatch = {
+                    action: loginAction.setProductMetricsGenerated,
+                    payload: value,
+                };
+                const state = loginReducer_setProductMetricsGenerated(
+                    initialLoginState,
+                    dispatch as any,
+                );
+                expect(state.productMetricsGenerated).toBe(
+                    initialProductMetricsGenerated,
+                );
+            });
+        });
+    });
+
+    describe(loginAction.setProductMetricsWorker, () => {
+        it("should allow valid Worker values", () => {
+            const worker = new Worker("");
+            const dispatch = {
+                action: loginAction.setProductMetricsWorker,
+                payload: worker,
+            };
+            const state = loginReducer_setProductMetricsWorker(
+                initialLoginState,
+                dispatch,
+            );
+            expect(state.productMetricsWorker).toBeInstanceOf(Worker);
+        });
+
+        it("should not allow invalid Worker values", () => {
+            const worker = {} as Worker;
+            const dispatch = {
+                action: loginAction.setProductMetricsWorker,
+                payload: worker,
+            };
+            const state = loginReducer_setProductMetricsWorker(
+                initialLoginState,
+                dispatch,
+            );
+            expect(state.productMetricsWorker).toBeNull();
+        });
+    });
+
+    describe(loginAction.setRepairMetricsGenerated, () => {
+        it("should allow valid boolean values", () => {
+            VALID_BOOLEANS.forEach((value) => {
+                const dispatch = {
+                    action: loginAction.setRepairMetricsGenerated,
+                    payload: value,
+                };
+                const state = loginReducer_setRepairMetricsGenerated(
+                    initialLoginState,
+                    dispatch,
+                );
+                expect(state.repairMetricsGenerated).toBe(value);
+            });
+        });
+
+        it("should not allow invalid boolean values", () => {
+            const initialRepairMetricsGenerated =
+                initialLoginState.repairMetricsGenerated;
+
+            INVALID_BOOLEANS.forEach((value) => {
+                const dispatch = {
+                    action: loginAction.setRepairMetricsGenerated,
+                    payload: value,
+                };
+                const state = loginReducer_setRepairMetricsGenerated(
+                    initialLoginState,
+                    dispatch as any,
+                );
+                expect(state.repairMetricsGenerated).toBe(
+                    initialRepairMetricsGenerated,
+                );
+            });
+        });
+    });
+
+    describe(loginAction.setRepairMetricsWorker, () => {
+        it("should allow valid Worker values", () => {
+            const worker = new Worker("");
+            const dispatch = {
+                action: loginAction.setRepairMetricsWorker,
+                payload: worker,
+            };
+            const state = loginReducer_setRepairMetricsWorker(
+                initialLoginState,
+                dispatch,
+            );
+            expect(state.repairMetricsWorker).toBeInstanceOf(Worker);
+        });
+
+        it("should not allow invalid Worker values", () => {
+            const worker = {} as Worker;
+            const dispatch = {
+                action: loginAction.setRepairMetricsWorker,
+                payload: worker,
+            };
+            const state = loginReducer_setRepairMetricsWorker(
+                initialLoginState,
+                dispatch,
+            );
+            expect(state.repairMetricsWorker).toBeNull();
         });
     });
 });
